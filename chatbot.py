6f3027a1a3669fe2447587938a29455111eb4764
saisathvigaa <52745477+saisathvigaa@users.noreply.github.com>
2023-08-03 20:22:52 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,7 +6,7 @@ import fitz
 app = Flask(__name__)
 
 # Set your OpenAI API key
-openai.api_key = "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"
+openai.api_key = "APIkey "
 
 def extract_pdf_text(pdf_path):
     text = ""
